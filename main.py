e88e6407c5cd35113d685f0960d1aa0c79ea9d15
Ruslan Vodopyanov <deadlayman@icloud.com>
2023-06-29 15:16:42 +0300

    Task 2: Add method in Student. Change Mentor. Add method in Reviewer

--- main.py
+++ main.py
@@ -7,12 +7,32 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lector(self, lector, course, grade):
+        if grade > 10:
+            print('Оценка должна быть по 10-бальной шкале')
+        if isinstance(lector, Lecturer) and course in lector.courses_attached and course in self.courses_in_progress:
+            if course in lector.lector_grades:
+                lector.lector_grades[course] += [grade]
+            else:
+                lector.lector_grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.lector_grades = {}
+
+
+class Reviewer(Mentor):
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -22,24 +42,3 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
-
-class Lecturer(Mentor):
-    pass
-
-
-class Reviewer(Mentor):
-    pass
-
-
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
-print(best_student.grades)
